942341cbc36e078c2e8f422aaf227f3f94604a6a
griffin huth <griffinhuth@griffins-MacBook-Pro-3.local>
2023-01-20 14:35:53 -0700

    pushing style fix

--- src/index.js
+++ src/index.js
@@ -425,7 +425,7 @@ const DateRangePicker = ({
     marginLeft: 10,
     marginRight:10,
     marginBottom: 10,
-  width:150}}
+  width:140}}
                       buttonTextStyle={buttonTextStyle}
                       onPress={dateJobsCall}
                     >
@@ -442,7 +442,7 @@ const DateRangePicker = ({
     marginLeft: 10,
     marginRight:10,
     marginBottom: 10,
-  width:150}}
+  width:140}}
                       buttonTextStyle={buttonTextStyle}
                       onPress={_onClose}
                     >
